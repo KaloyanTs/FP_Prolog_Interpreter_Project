2272aa981127d02553bd6b81dfe25320554cfcd4
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-01-14 20:48:28 +0200

    experimenting

---
+++ prolog/starwars2.pl
@@ -0,0 +1,25 @@
+father(ruwee, padme).
+father(anakin, luke).
+father(anakin, leia).
+father(han, ben).
+%father(X,Y) :- male(X), parent(X,Y).
+
+mother(jobal, padme).
+mother(shmi, anakin).
+mother(padme, luke).
+mother(padme, leia).
+mother(leia, ben).
+
+alias(darthvader, anakin).
+alias(kyloren, ben).
+alias(X,Y) :- alias(Y,X).
+
+parent(han, kyloren).
+parent(X,Y) :- father(X, Y).
+parent(X,Y) :- mother(X, Y).
+
+childof(X, Y) :- parent(Y ,X).
+
+son(X,Y) :- male(X), father(Y,X).
+
+male(han).
