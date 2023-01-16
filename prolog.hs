26de235419e685fe3d463c43b28a703390207ec9
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-01-16 17:13:50 +0200

    excess file

--- prolog.hs
+++
@@ -1,7 +0,0 @@
-module Prolog (module Datatypes, module Checkers, module Conversions, module Resolution, module Unification) where
-
-import Checkers
-import Conversions
-import Datatypes
-import Resolution
-import Unification
